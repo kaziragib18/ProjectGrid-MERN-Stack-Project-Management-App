b8d8594c620ee4830cac29a97843daf4102e6ad7
Kazi Md. Ragib <kaziragib18@gmail.com>
2025-08-04 21:22:47 +0600

    updated files

--- backend/models/user.js
+++ backend/models/user.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: trim, // Automatically removes whitespace
+      trim: trim,
     },
     profilePicture: {
       type: String,
